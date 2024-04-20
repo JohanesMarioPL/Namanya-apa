4bfcb531e4bf8745afbabd5fe07cb63420ea2c8e
JohanesMarioPL <113487294+JohanesMarioPL@users.noreply.github.com>
2024-04-20 17:47:20 +0700

    Add files via upload

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use Cassandra\Smallint;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        static $nrp = 2272000;
+
+        $emailDomain = '@maranatha.ac.id';
+        $nrp++;
+        return [
+            'nrp' => $nrp,
+            'name' => $this->faker->name,
+            'email' => $nrp . $emailDomain,
+            'password' => bcrypt('12345678'),
+            'role_id' => 3,
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
